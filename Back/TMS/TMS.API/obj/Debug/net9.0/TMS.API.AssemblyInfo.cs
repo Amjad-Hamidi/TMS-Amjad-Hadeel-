e529a3f7d21231c9d80397bc49bfd294632134c1
Amjad-Hamidi <amjada871@yahoo.com>
2025-04-19 20:22:17 +0300

    Reordering files

--- Back/TMS/TMS.API/obj/Debug/net9.0/TMS.API.AssemblyInfo.cs
+++ Back/TMS/TMS.API/obj/Debug/net9.0/TMS.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TMS.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b779f3893e20e10fd1d5dbf98dfffd1e42319991")]
 [assembly: System.Reflection.AssemblyProductAttribute("TMS.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TMS.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
