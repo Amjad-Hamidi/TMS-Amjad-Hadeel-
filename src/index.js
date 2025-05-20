f047013a944a1f9a52219890a221f66ab4f023a7
Amjad-Hamidi <amjada871@yahoo.com>
2025-05-21 02:36:35 +0300

    clean Front folder and to push into Front branch

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "@fortawesome/fontawesome-free/css/all.min.css"; // استيراد أيقونات Font Awesome
-import App from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
