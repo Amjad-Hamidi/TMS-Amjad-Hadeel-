e43abd0fa798708c4cf220f75267b29c79c1eaba
Amjad-Hamidi <amjada871@yahoo.com>
2025-06-06 21:03:25 +0300

    V2 fix Back folder

--- Back Work/TMS/TMS.API/obj/Debug/net9.0/TMS.API.AssemblyInfo.cs
+++ Back Work/TMS/TMS.API/obj/Debug/net9.0/TMS.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TMS.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa00c6a4fa0c2cdd5f5c51f533dc3c3434d21600")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+676e2a625991992ddd20dc02f473c6de9d961494")]
 [assembly: System.Reflection.AssemblyProductAttribute("TMS.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TMS.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
